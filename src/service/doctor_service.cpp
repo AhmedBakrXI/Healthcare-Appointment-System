c39cdab9775d28ab3f5c00aaddf485bc3e23034a
a7med31 <ahmed.abdulrahman3000@gmail.com>
2024-07-26 18:31:54 +0300

    Update doctor_service.cpp

--- src/service/doctor_service.cpp
+++ src/service/doctor_service.cpp
@@ -1,4 +1,4 @@
-#include "doctorService.h"
+#include "doctor_service.h"
 #include <iostream>
 
 // Constructor to initialize the doctor service with a doctor and DB manager
@@ -24,15 +24,37 @@ void doctorService::viewSchedule() {
 	for (const auto &appointment : appointments) {
 		std::cout << "Appointment ID: " << appointment.getId() << "\n"
 		          << "Date: " << appointment.getDate() << "\n"
-		          << "Patient ID: " << (appointment.getPatient().has_value() ? appointment.getPatient()->getID() : 0)
 		          << "\n\n";
 	}
 }
 
 // Function to update the doctor's schedule
-bool doctorService::updateSchedule(const Schedule &schedule) {
+bool doctorService::updateSchedule(const Schedule &newSchedule) {
+
+	if (dbManager.updateDoctorSchedule(doctor.getID(), newSchedule)) {
+		std::cout << "Schedule updated successfully!" << std::endl;
+		return true;
+	} else {
+		std::cerr << "Failed to update schedule!" << std::endl;
+		return false;
+	}
 }
 
 // Function to update a patient's medical record
 bool doctorService::updateMedicalRecord(const Medical_Record &record) {
+	Medical_Record optionalRecord = dbManager.getMedicalRecord(record.getID());
+
+	if (!optionalRecord.has_value()) {
+		std::cerr << "Record not found!" << std::endl;
+		return false;
+	}
+
+	// Update the medical record in the database
+	if (dbManager.updateMedicalRecord(record.getId(), record)) {
+		std::cout << "Medical record updated successfully!" << std::endl;
+		return true;
+	} else {
+		std::cerr << "Failed to update medical record!" << std::endl;
+		return false;
+	}
 }
