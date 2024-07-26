47f4b0dd1c6aa2a320b301395f0cad5b447ea30f
a7med31 <ahmed.abdulrahman3000@gmail.com>
2024-07-26 17:14:34 +0300

    update patient service

--- src/service/patient_service.cpp
+++ src/service/patient_service.cpp
@@ -1,47 +1,53 @@
 #include "patient_service.h"
-#include "../database/db_manager.h"
 #include <iostream>
 
-using namespace std;
-
 // Constructor to initialize the patient
-PatientService::PatientService(const Patient &patient) : patient(patient) {
+PatientService::PatientService(const Patient &patient, DBManager &dbManager) : patient(patient), dbManager(dbManager) {
 }
 
 // Function to view the medical history of the patient
 void PatientService::viewMedicalHistory() {
-	Medical_History history = patient.gethistory();
-	vector<Medical_Record> records = history.getMedicalHistory();
 
-	cout << "Medical History:\n";
+	Medical_History history = patient.gethistory();
+	std::vector<Medical_Record> records = history.getMedicalHistory();
+
+	std::cout << "Medical History:\n";
 	for (const auto &record : records) {
-		cout << "Record ID: " << record.getId() << "\n"
-		     << "Date: " << record.getDate() << "\n"
-		     << "Description: " << record.getDescription() << "\n\n";
+		std::cout << "Record ID: " << record.getId() << "\n"
+		          << "Date: " << record.getDate() << "\n"
+		          << "Description: " << record.getDescription() << "\n\n";
 	}
 }
 
 // Function to get the medical history of the patient
 Medical_History PatientService::getMedicalHistory() {
+
 	return patient.gethistory();
 }
 
 // Function to book an appointment for the patient with a specified doctor
 bool PatientService::bookAppointment(int doctorId, const Appointment &appointment) {
 
-	Doctor doctor = getDoctor(int id);
+	std::optional<Doctor> optionalDoctor = dbManager.getDoctor(doctorId);
+
+	if (!optionalDoctor.has_value()) {
+		std::cerr << "Doctor not found!" << std::endl;
+		return false;
+	}
+
+	Doctor doctor = optionalDoctor.value();
 	Schedule doctorSchedule = doctor.getSchedule();
-	vector<Appointment> appointments = doctorSchedule.getAppointment();
+	std::vector<Appointment> appointments = doctorSchedule.getAppointment();
 
 	for (const auto &appt : appointments) {
 		if (appt.getDate() == appointment.getDate()) {
-			cerr << "Doctor is not available at the requested time!" << "\n";
+			std::cerr << "Doctor is not available at the requested time!" << std::endl;
 			return false;
 		}
 	}
 
 	doctorSchedule.addAppointment(appointment);
 
-	cout << "Appointment booked successfully!" << "\n";
+	std::cout << "Appointment booked successfully!" << std::endl;
 	return true;
 }
--- src/service/patient_service.h
+++ src/service/patient_service.h
@@ -1,3 +1,6 @@
+#pragma once
+
+#include "../database/db_manager.h"
 #include "../time/appointment.h"
 #include "../time/medical_history.h"
 #include "../user/patient.h"
@@ -6,10 +9,11 @@ class PatientService {
 
   private:
 	Patient patient;
+	DBManager &dbManager;
 
   public:
 	// Constructor to initialize the patient
-	PatientService(const Patient &patient);
+	PatientService(const Patient &patient, DBManager &dbManager);
 
 	// Function to view the medical history of the patient
 	void viewMedicalHistory();
@@ -18,5 +22,5 @@ class PatientService {
 	Medical_History getMedicalHistory();
 
 	// Function to book an appointment for the patient with a specified doctor
-	bool bookAppointment(int doctorId, Appointment appointment);
-}
+	bool bookAppointment(int doctorId, const Appointment &appointment);
+};
